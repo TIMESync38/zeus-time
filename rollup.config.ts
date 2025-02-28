f5d4b886cb78f83ffa558e85ebdeb6744eaf2756
o0CroMag0o <99709838+CroMagnonPlan@users.noreply.github.com>
2025-02-28 00:21:13 +0000

    FIX js map

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,18 +1,20 @@
 import typescript from "@rollup/plugin-typescript";
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
 
 export default {
   input: "src/index.ts",
   output: [
     {
-      file: "dist/index.js",
+      file: "dist/index.cjs",
       format: "cjs",
       sourcemap: true
     },
     {
-      file: "dist/index.esm.js",
+      file: "dist/index.mjs",
       format: "esm",
       sourcemap: true
     }
   ],
-  plugins: [typescript()]
+  plugins: [typescript(), commonjs(), resolve()]
 };
